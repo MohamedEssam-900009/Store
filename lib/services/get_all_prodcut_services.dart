e794d3f23a5ed4b10f9d738dbce0199bb2621c02
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-07 11:36:31 +0300

    added services all product service

--- lib/services/get_all_prodcut_services.dart
+++ lib/services/get_all_prodcut_services.dart
@@ -1,1 +1,19 @@
-class GetAllProducts {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/product_model.dart';
+
+class AllProductsServices {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    List<dynamic> data = jsonDecode(response.body);
+
+    List<ProductModel> productsList = [];
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
+    }
+    return productsList;
+  }
+}
