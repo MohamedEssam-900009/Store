eb0c6f63ebde8da55b76a22827e5bdae854623ad
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-08 12:39:02 +0300

    added status code to all services

--- lib/services/all_categories_services.dart
+++ lib/services/all_categories_services.dart
@@ -6,7 +6,12 @@ class AllCategories {
   Future<List<dynamic>> getAllCategories() async {
     http.Response response = await http
         .get(Uri.parse('https://fakestoreapi.com/products/categories'));
-    List<dynamic> data = jsonDecode(response.body);
-    return data;
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'there is problem with status code ${response.statusCode}');
+    }
   }
 }
