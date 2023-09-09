20ca250c5b6d1b3a48220f7463464a970898c8f1
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-09 18:07:35 +0300

    refactor all services

--- lib/services/all_categories_services.dart
+++ lib/services/all_categories_services.dart
@@ -1,17 +1,10 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:store/helper/api.dart';
 
 class AllCategories {
   Future<List<dynamic>> getAllCategories() async {
-    http.Response response = await http
-        .get(Uri.parse('https://fakestoreapi.com/products/categories'));
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-          'there is problem with status code ${response.statusCode}');
-    }
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products/categories');
+
+    return data;
   }
 }
