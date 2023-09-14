fdb23a5d00f4bce6e116d92bdf7bbc2c2ed66162
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-14 11:33:02 +0300

    home page ui

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static String id = 'HomePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.black,
+            ),
+          ),
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
