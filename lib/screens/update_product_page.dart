edac5fdacca4857016537f73605fa8747eda3b99
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-23 02:51:22 +0300

    loading indicator and handling exception

--- lib/screens/update_product_page.dart
+++ lib/screens/update_product_page.dart
@@ -1,92 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:store/models/product_model.dart';
 import 'package:store/services/update_product.dart';
+import 'package:store/widgets/custom_button.dart';
 import 'package:store/widgets/custom_text_fields.dart';
-import 'package:store/widgets/custome_button.dart';
 
-class UpdateProductPage extends StatelessWidget {
-  UpdateProductPage({super.key});
+class UpdateProductPage extends StatefulWidget {
+  const UpdateProductPage({super.key});
 
   static String id = 'update product';
+
+  @override
+  State<UpdateProductPage> createState() => _UpdateProductPageState();
+}
+
+class _UpdateProductPageState extends State<UpdateProductPage> {
   String? productName, description, image, price;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     ProductModel product =
         ModalRoute.of(context)!.settings.arguments as ProductModel;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Update Product',
-          style: TextStyle(
-            color: Colors.black,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Update Product',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100.0,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    productName = data;
+                  },
+                  hintText: 'Product Name',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    description = data;
+                  },
+                  hintText: 'Description',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    price = data;
+                  },
+                  inputType: TextInputType.number,
+                  hintText: 'Price',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  onChanged: (data) {
+                    image = data;
+                  },
+                  hintText: 'Image',
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                CustomButton(
+                  text: 'Update',
+                  onTap: () {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                      updateProduct(product);
+                      print('Success');
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                    isLoading = false;
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100.0,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  productName = data;
-                },
-                hintText: 'Product Name',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  description = data;
-                },
-                hintText: 'Description',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  price = data;
-                },
-                inputType: TextInputType.number,
-                hintText: 'Price',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              CustomTextField(
-                onChanged: (data) {
-                  image = data;
-                },
-                hintText: 'Image',
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              CustomButton(
-                text: 'Update',
-                onTap: () {
-                  UpdateProductService().updateProduct(
-                    title: productName!,
-                    price: price!,
-                    description: description!,
-                    image: image!,
-                    category: product.category,
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+    );
+  }
+
+  void updateProduct(ProductModel product) {
+    UpdateProductService().updateProduct(
+      title: productName!,
+      price: price!,
+      description: description!,
+      image: image!,
+      category: product.category,
     );
   }
 }
